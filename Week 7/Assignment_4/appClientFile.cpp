b179379179aa1501c3ab979e1e3fe48d488d5262
ShirleyZam <shirleyzamora1018@gmail.com>
2025-11-09 17:46:56 -0500

    Implement client data input and file appending
    Add functionality to append client data to a file.

--- Week 7/Assignment_4/appClientFile.cpp
+++ Week 7/Assignment_4/appClientFile.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
+using namespace std;
+
+int main(){
+    ofstream outClientFile("client.txt", ios::app);
+
+    if(!outClientFile){
+        cerr << "File could not be opened.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Enter account, name, and balance. (space seperated)\n"
+    << "Enter end-of-file to end input. \n? ";
+
+    int account;
+    string name;
+    double balance;
+
+    while(cin >> account >> name >> balance){
+        outClientFile << account << ' ' << name << ' ' << balance << endl;
+        cout << "? ";
+    } 
+
+    return 0;
+}
