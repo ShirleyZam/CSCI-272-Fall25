0039dcd7f77d999de8a21f26195474fd8df46383
ShirleyZam <shirleyzamora1018@gmail.com>
2025-11-09 17:43:43 -0500

    Add files via upload

---
+++ Week 7/Assignment_4/outClientFile.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    ofstream outClientFile("client.txt", ios::out);
+
+    if(!outClientFile){
+        cerr << "File could not be opened.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Enter account, name, and balance. (space seperated)\n"
+    << "Enter end-of-file to end input. \n? ";
+
+    int account;
+    string name;
+    double balance;
+
+    while(cin >> account >> name >> balance){
+        outClientFile << account << ' ' << name << ' ' << balance << endl;
+        cout << "? ";
+    } 
+
+    return 0;
+}
