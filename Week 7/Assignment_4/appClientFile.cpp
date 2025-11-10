c76056d6a72cd389aca5d379bd117f9656dcf609
ShirleyZam <shirleyzamora1018@gmail.com>
2025-11-09 23:21:59 -0500

    Enhance input handling with error checking
    Refactor input handling to improve user experience and error checking.

--- Week 7/Assignment_4/appClientFile.cpp
+++ Week 7/Assignment_4/appClientFile.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <limits>
 #include <cstdlib>
 
 using namespace std;
@@ -13,16 +14,28 @@ int main(){
     }
 
     cout << "Enter account, name, and balance. (space seperated)\n"
-    << "Enter end-of-file to end input. \n? ";
+    << "Enter Ctrl+Z (Windows) or Ctrl+D(Mac) to end input. \n? ";
 
     int account;
     string name;
     double balance;
 
-    while(cin >> account >> name >> balance){
-        outClientFile << account << ' ' << name << ' ' << balance << endl;
-        cout << "? ";
+    while(true){
+        if(cin >> account >> name >> balance){
+            outClientFile << account << ' ' << name << ' ' << balance << endl;
+            cout << "? ";
+        }else if(cin.eof()){
+            break;
+        }else{
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            cout << "Invalid input \n"; 
+        }
+        
+        
+        
+        
     } 
-
-    return 0;
+    outClientFile.close();
+    
 }
