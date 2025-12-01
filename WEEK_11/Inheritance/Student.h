1635c58a5e34c727bb4fb1afd546bfaf2313792b
ShirleyZam <shirleyzamora1018@gmail.com>
2025-12-01 09:09:42 -0500

    Add files via upload

---
+++ WEEK_11/Inheritance/Student.h
@@ -0,0 +1,27 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include<iostream>
+
+
+#include "Person.h"
+
+class Student : public Person {
+	private: 
+		int studentID; 
+	public:
+		//base constructor called
+		
+	Student(string n, int id) : Person(n), studentID(id){
+		cout << " Student constructor\n";
+	
+	}
+	
+	~Student(){
+		cout << " Student Destructor\n";
+	}
+	
+	void showInfo() const{
+		showName(); 
+		cout << "ID: " << studentID << endl; 
+	}
+};
---
+++ WEEK_11/main.cpp
@@ -0,0 +1,9 @@
+//main.cpp
+#include "Student.h"
+
+int main(){
+	Student samuel("Samuel Ruben", 123432);
+	samuel.showName();
+	
+	return 0; 
+}
